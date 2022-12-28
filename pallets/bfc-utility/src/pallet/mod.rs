aa482f32b3d2ca592614dd4852ef6fc7f43c3a71
dnjscksdn98 <dnjscksdn98@gmail.com>
2022-12-28 11:18:53 +0900

    migrate pallet-bfc-utility

--- pallets/bfc-utility/src/pallet/mod.rs
+++ pallets/bfc-utility/src/pallet/mod.rs
@@ -25,7 +25,7 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Overarching event type.
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		/// The currency type.
 		type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
 		/// The origin which may forcibly mint native tokens.
