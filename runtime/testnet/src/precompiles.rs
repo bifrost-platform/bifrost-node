94722d046182882c7c0d011ebcd006aeb3344466
dnjscksdn98 <dnjscksdn98@gmail.com>
2022-12-29 14:47:32 +0900

    migrate bifrost precompiles

--- runtime/testnet/src/precompiles.rs
+++ runtime/testnet/src/precompiles.rs
@@ -1,15 +1,23 @@
-// //! The BIFROST Network EVM precompiles. This can be compiled with `#[no_std]`, ready for Wasm.
+// Copyright 2019-2022 PureStake Inc.
+// This file is part of Moonbeam.
 
-// use crate::{CouncilInstance, TechCommitteeInstance};
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
-// use codec::Decode;
-// use frame_support::dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo};
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
-// use pallet_evm::{Context, Precompile, PrecompileResult, PrecompileSet};
-// use pallet_evm_precompile_blake2::Blake2F;
-// use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
-// use pallet_evm_precompile_modexp::Modexp;
-// use pallet_evm_precompile_simple::{ECRecover, Identity, Ripemd160, Sha256};
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
+use pallet_evm_precompile_blake2::Blake2F;
+use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
+use pallet_evm_precompile_modexp::Modexp;
+use pallet_evm_precompile_simple::{ECRecover, Identity, Ripemd160, Sha256};
 
 // use precompile_balance::BalancePrecompile;
 // use precompile_bfc_offences::BfcOffencesPrecompile;
@@ -18,87 +26,42 @@
 // use precompile_governance::GovernancePrecompile;
 // use precompile_relay_manager::RelayManagerPrecompile;
 
-// use sp_core::H160;
-// use sp_std::marker::PhantomData;
+use precompile_utils::precompile_set::*;
 
-// /// The PrecompileSet installed in the BIFROST runtime
-// #[derive(Debug, Clone, Copy)]
-// pub struct BifrostPrecompiles<R>(PhantomData<R>);
-
-// impl<R> BifrostPrecompiles<R> {
-// 	pub fn new() -> Self {
-// 		Self(Default::default())
-// 	}
-
-// 	pub fn used_addresses() -> impl Iterator<Item = H160> {
-// 		sp_std::vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 1024, 1280, 2048, 2049, 2050, 4096, 8192]
-// 			.into_iter()
-// 			.map(|x| hash(x))
-// 	}
-// }
-
-// impl<R> PrecompileSet for BifrostPrecompiles<R>
-// where
-// 	BfcStakingPrecompile<R>: Precompile,
-// 	BfcOffencesPrecompile<R>: Precompile,
-// 	RelayManagerPrecompile<R>: Precompile,
-// 	GovernancePrecompile<R>: Precompile,
-// 	CollectivePrecompile<R, CouncilInstance>: Precompile,
-// 	CollectivePrecompile<R, TechCommitteeInstance>: Precompile,
-// 	BalancePrecompile<R>: Precompile,
-// 	R: pallet_evm::Config,
-// 	<R::Call as Dispatchable>::Origin: From<Option<R::AccountId>>,
-// 	R::Call: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo + Decode,
-// {
-// 	fn execute(
-// 		&self,
-// 		address: H160,
-// 		input: &[u8],
-// 		target_gas: Option<u64>,
-// 		context: &Context,
-// 		is_static: bool,
-// 	) -> Option<PrecompileResult> {
-// 		match address {
-// 			// Standard Ethereum precompiles
-// 			a if a == hash(1) => Some(ECRecover::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(2) => Some(Sha256::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(3) => Some(Ripemd160::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(4) => Some(Identity::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(5) => Some(Modexp::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(6) => Some(Bn128Add::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(7) => Some(Bn128Mul::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(8) => Some(Bn128Pairing::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(9) => Some(Blake2F::execute(input, target_gas, context, is_static)),
-
-// 			// BIFROST custom precompiles
-// 			a if a == hash(1024) =>
-// 				Some(BfcStakingPrecompile::<R>::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(1280) =>
-// 				Some(BfcOffencesPrecompile::<R>::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(2048) =>
-// 				Some(GovernancePrecompile::<R>::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(2049) => Some(CollectivePrecompile::<R, CouncilInstance>::execute(
-// 				input, target_gas, context, is_static,
-// 			)),
-// 			a if a == hash(2050) =>
-// 				Some(CollectivePrecompile::<R, TechCommitteeInstance>::execute(
-// 					input, target_gas, context, is_static,
-// 				)),
-// 			a if a == hash(4096) =>
-// 				Some(BalancePrecompile::<R>::execute(input, target_gas, context, is_static)),
-// 			a if a == hash(8192) =>
-// 				Some(RelayManagerPrecompile::<R>::execute(input, target_gas, context, is_static)),
-
-// 			// Default
-// 			_ => None,
-// 		}
-// 	}
-
-// 	fn is_precompile(&self, address: H160) -> bool {
-// 		Self::used_addresses().any(|x| x == address)
-// 	}
-// }
-
-// fn hash(a: u64) -> H160 {
-// 	H160::from_low_u64_be(a)
-// }
+/// The PrecompileSet installed in the Moonbeam runtime.
+/// We include the nine Istanbul precompiles
+/// (https://github.com/ethereum/go-ethereum/blob/3c46f557/core/vm/contracts.go#L69)
+/// as well as a special precompile for dispatching Substrate extrinsics
+/// The following distribution has been decided for the precompiles
+/// 0-1023: Ethereum Mainnet Precompiles
+/// 1024-8192 Moonbeam specific precompiles
+pub type BifrostPrecompiles<R> = PrecompileSetBuilder<
+	R,
+	(
+		// Skip precompiles if out of range.
+		PrecompilesInRangeInclusive<
+			(AddressU64<1>, AddressU64<4095>),
+			(
+				// Ethereum precompiles:
+				// We allow DELEGATECALL to stay compliant with Ethereum behavior.
+				PrecompileAt<AddressU64<1>, ECRecover, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<2>, Sha256, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<3>, Ripemd160, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<4>, Identity, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<5>, Modexp, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<6>, Bn128Add, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<7>, Bn128Mul, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<8>, Bn128Pairing, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<9>, Blake2F, ForbidRecursion, AllowDelegateCall>,
+				// Moonbeam specific precompiles:
+				// PrecompileAt<AddressU64<1024>, BfcStakingPrecompile<R>>,
+				// PrecompileAt<AddressU64<1280>, BfcOffencesPrecompile<R>>,
+				// PrecompileAt<AddressU64<2048>, GovernancePrecompile<R>>,
+				// PrecompileAt<AddressU64<2049>, CollectivePrecompile<R, CouncilInstance>>,
+				// PrecompileAt<AddressU64<2050>, CollectivePrecompile<R, TechCommitteeInstance>>,
+				// PrecompileAt<AddressU64<4096>, BalancePrecompile<R>>,
+				// PrecompileAt<AddressU64<8192>, RelayManagerPrecompile<R>>,
+			),
+		>,
+	),
+>;
