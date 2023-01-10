c092b5771a0466577b85047dbb492b339ab46f7f
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-01-10 15:57:37 +0900

    release bifrost-node v1.1.6

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -140,7 +140,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 425,
+	spec_version: 430,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
