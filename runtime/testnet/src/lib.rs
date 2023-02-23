4d26c693edaca52cad72d6ce56aa85de277156d7
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-02-23 10:53:56 +0900

    release bifrost-node v1.2.0

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -110,6 +110,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
+	MigrateRelayManager,
 >;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
@@ -141,7 +142,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 445,
+	spec_version: 450,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
@@ -695,6 +696,15 @@ impl pallet_relay_manager::Config for Runtime {
 	type IsHeartbeatOffenceActive = IsHeartbeatOffenceActive;
 	type DefaultHeartbeatSlashFraction = DefaultHeartbeatSlashFraction;
 	type WeightInfo = pallet_relay_manager::weights::SubstrateWeight<Runtime>;
+}
+
+pub struct MigrateRelayManager;
+impl frame_support::traits::OnRuntimeUpgrade for MigrateRelayManager {
+	fn on_runtime_upgrade() -> Weight {
+		pallet_relay_manager::migrations::v3::pre_migrate::<Runtime>().ok();
+		let weight = pallet_relay_manager::migrations::v3::migrate::<Runtime>();
+		weight
+	}
 }
 
 parameter_types! {
