62914453dd902245dafd3706e458d5c218464172
dnjscksdn98 <dnjscksdn98@gmail.com>
2024-03-14 19:56:02 +0900

    NODE-99, chore: init pallet

---
+++ pallets/btc-socket-queue/src/pallet/mod.rs
@@ -0,0 +1,25 @@
+mod impls;
+
+use frame_support::{pallet_prelude::*, traits::StorageVersion};
+use frame_system::pallet_prelude::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
+	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Overarching event type
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
+	pub enum Event<T: Config> {}
+}
