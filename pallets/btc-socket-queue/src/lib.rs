136b7307c9fe3847a06dcef69cc972c53c00a137
dnjscksdn98 <dnjscksdn98@gmail.com>
2024-03-18 18:45:48 +0900

    NODE-99, feat: init pallet

--- pallets/btc-socket-queue/src/lib.rs
+++ pallets/btc-socket-queue/src/lib.rs
@@ -2,3 +2,36 @@
 
 mod pallet;
 pub use pallet::pallet::*;
+
+pub mod weights;
+use weights::WeightInfo;
+
+use parity_scale_codec::{Decode, Encode};
+use scale_info::TypeInfo;
+use sp_std::vec::Vec;
+
+pub const MAX_QUEUE_SIZE: u32 = 1_000;
+
+/// The maximum amount of accounts a multi-sig account can consist.
+pub const MULTI_SIG_MAX_ACCOUNTS: u32 = 16;
+
+pub type ReqId = u128;
+
+#[derive(Decode, Encode, TypeInfo)]
+pub struct UnsignedPsbtMessage {
+	pub req_id: ReqId,
+	pub psbt: Vec<u8>,
+}
+
+#[derive(Decode, Encode, TypeInfo)]
+pub struct SignedPsbtMessage<AccountId> {
+	pub psbt: Vec<u8>,
+	pub authority_id: AccountId,
+	pub status: SignStatus,
+}
+
+#[derive(Decode, Encode, TypeInfo)]
+pub enum SignStatus {
+	Accepted,
+	Rejected,
+}
--- pallets/btc-socket-queue/src/pallet/mod.rs
+++ pallets/btc-socket-queue/src/pallet/mod.rs
@@ -1,7 +1,14 @@
 mod impls;
 
+use crate::{
+	ReqId, SignedPsbtMessage, UnsignedPsbtMessage, WeightInfo, MAX_QUEUE_SIZE,
+	MULTI_SIG_MAX_ACCOUNTS,
+};
+
 use frame_support::{pallet_prelude::*, traits::StorageVersion};
 use frame_system::pallet_prelude::*;
+
+use sp_runtime::traits::{IdentifyAccount, Verify};
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -17,9 +24,56 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		/// Overarching event type
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		/// The signature signed by the issuer.
+		type Signature: Verify<Signer = Self::Signer> + Encode + Decode + Parameter;
+		/// The signer of the message.
+		type Signer: IdentifyAccount<AccountId = Self::AccountId>
+			+ Encode
+			+ Decode
+			+ Parameter
+			+ MaxEncodedLen;
+		/// Weight information for extrinsics in this pallet.
+		type WeightInfo: WeightInfo;
 	}
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
 	pub enum Event<T: Config> {}
+
+	#[pallet::storage]
+	#[pallet::getter(fn signer)]
+	/// The signer address. The signature is originated from this account.
+	pub type Signer<T: Config> = StorageValue<_, T::Signer, OptionQuery>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	#[pallet::getter(fn queue)]
+	pub type Queue<T: Config> =
+		StorageValue<_, BoundedVec<UnsignedPsbtMessage, ConstU32<MAX_QUEUE_SIZE>>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	#[pallet::getter(fn pending_psbt)]
+	pub type PendingPsbt<T: Config> =
+		StorageMap<_, Twox64Concat, ReqId, SignedPsbtMessage<T::AccountId>>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	#[pallet::getter(fn accepted_psbt)]
+	pub type AcceptedPsbt<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		ReqId,
+		BoundedVec<SignedPsbtMessage<T::AccountId>, ConstU32<MULTI_SIG_MAX_ACCOUNTS>>,
+	>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	#[pallet::getter(fn rejected_psbt)]
+	pub type RejectedPsbt<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		ReqId,
+		BoundedVec<SignedPsbtMessage<T::AccountId>, ConstU32<MULTI_SIG_MAX_ACCOUNTS>>,
+	>;
 }
