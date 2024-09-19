9414b0981e76782ee8846dfbdd083521c476761c
dnjscksdn98 <dnjscksdn98@gmail.com>
2024-09-19 13:15:21 +0900

    chore: set mcpu for mac

--- runtime/dev/build.rs
+++ runtime/dev/build.rs
@@ -1,7 +1,9 @@
 fn main() {
 	#[cfg(feature = "std")]
 	{
-		// std::env::set_var("CFLAGS", "-mcpu=mvp");
+		#[cfg(target_arch = "aarch64")]
+		std::env::set_var("CFLAGS", "-mcpu=mvp");
+
 		substrate_wasm_builder::WasmBuilder::new()
 			.with_current_project()
 			.export_heap_base()
