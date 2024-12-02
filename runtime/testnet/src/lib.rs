477c4a0cf219407895d8aa21c621a16d74c2acf0
dnjscksdn98 <dnjscksdn98@gmail.com>
2024-12-02 14:51:13 +0900

    chore: remove passed storage migrations

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -112,10 +112,7 @@ pub type UncheckedExtrinsic =
 	fp_self_contained::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
 
 /// All migrations executed on runtime upgrade as a nested tuple of types implementing `OnRuntimeUpgrade`.
-type Migrations = (
-	pallet_identity::migration::versioned::V0ToV1<Runtime, { u64::MAX }>,
-	pallet_grandpa::migrations::MigrateV4ToV5<Runtime>,
-);
+type Migrations = ();
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
