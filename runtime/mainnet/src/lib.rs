e244a68989f1a4d407b291e4b6ceb442ac06f8a0
dnjscksdn98 <dnjscksdn98@gmail.com>
2024-12-11 14:51:43 +0900

    NODE-158, deps: increase runtime version

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -148,7 +148,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 2027,
+	spec_version: 2028,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
