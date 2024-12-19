420acb4ac31867456f155d0433c3ed319abb62b4
dnjscksdn98 <dnjscksdn98@gmail.com>
2024-12-19 13:24:04 +0900

    NODE-149, feat: init bfc-staking v6 storage migration

--- runtime/dev/src/lib.rs
+++ runtime/dev/src/lib.rs
@@ -111,10 +111,7 @@ pub type UncheckedExtrinsic =
 	fp_self_contained::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
 
 /// All migrations executed on runtime upgrade as a nested tuple of types implementing `OnRuntimeUpgrade`.
-type Migrations = (
-	pallet_identity::migration::versioned::V0ToV1<Runtime, { u64::MAX }>,
-	pallet_grandpa::migrations::MigrateV4ToV5<Runtime>,
-);
+type Migrations = ();
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
@@ -154,7 +151,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 371,
+	spec_version: 372,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
