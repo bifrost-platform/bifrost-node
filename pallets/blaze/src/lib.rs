f99d062f6067aed98723461d8933e040945ddf71
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-04-09 13:43:30 +0900

    NODE-179, feat: init pallet storages

--- pallets/blaze/src/lib.rs
+++ pallets/blaze/src/lib.rs
@@ -4,4 +4,36 @@ mod pallet;
 pub mod weights;
 
 pub use pallet::pallet::*;
+use sp_runtime::BoundedBTreeMap;
 pub use weights::WeightInfo;
+
+use parity_scale_codec::{Decode, Encode};
+use scale_info::TypeInfo;
+use sp_core::{ConstU32, RuntimeDebug, H256, U256};
+
+use bp_staking::MAX_AUTHORITIES;
+
+/// The round of the registration pool.
+pub type PoolRound = u32;
+
+#[derive(Decode, Encode, TypeInfo, Clone, PartialEq, Eq, RuntimeDebug)]
+pub struct Utxo<AccountId> {
+	pub txid: H256,
+	pub vout: U256,
+	pub amount: U256,
+	pub is_approved: bool,
+	// TODO: need lock time
+	pub votes: BoundedBTreeMap<AccountId, bool, ConstU32<MAX_AUTHORITIES>>,
+}
+
+#[derive(Decode, Encode, TypeInfo, Clone, PartialEq, Eq, RuntimeDebug)]
+pub struct PendingFeeRate<AccountId> {
+	pub is_approved: bool,
+	pub votes: BoundedBTreeMap<AccountId, U256, ConstU32<MAX_AUTHORITIES>>,
+}
+
+impl<AccountId: Ord> Default for PendingFeeRate<AccountId> {
+	fn default() -> Self {
+		Self { is_approved: false, votes: Default::default() }
+	}
+}
--- pallets/blaze/src/pallet/mod.rs
+++ pallets/blaze/src/pallet/mod.rs
@@ -1,8 +1,11 @@
-use crate::weights::WeightInfo;
+use crate::{weights::WeightInfo, PendingFeeRate, PoolRound, Utxo};
 
 use frame_support::{pallet_prelude::*, traits::StorageVersion};
 use frame_system::pallet_prelude::*;
 
+use bp_btc_relay::UnboundedBytes;
+use sp_core::H256;
+use sp_std::vec::Vec;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
@@ -29,4 +32,57 @@ pub mod pallet {
 	impl<T: Config> BuildGenesisConfig for GenesisConfig<T> {
 		fn build(&self) {}
 	}
+
+	#[pallet::storage]
+	/// The flag that represents whether BLAZE is activated.
+	pub type IsActivated<T: Config> = StorageValue<_, bool, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	/// key: pool round
+	/// value: utxo(s)
+	pub type Utxos<T: Config> =
+		StorageMap<_, Twox64Concat, PoolRound, Vec<Utxo<T::AccountId>>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	/// key: txid
+	/// value: utxo(s)
+	pub type LockedTxos<T: Config> = StorageDoubleMap<
+		_,
+		Twox64Concat,
+		PoolRound,
+		Twox64Concat,
+		H256,
+		Vec<Utxo<T::AccountId>>,
+		ValueQuery,
+	>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	/// key: txid
+	/// value: utxo(s)
+	pub type SpentTxos<T: Config> = StorageDoubleMap<
+		_,
+		Twox64Concat,
+		PoolRound,
+		Twox64Concat,
+		H256,
+		Vec<Utxo<T::AccountId>>,
+		ValueQuery,
+	>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	/// key: pool round
+	/// value: pending outbound requests socket messages (in bytes)
+	pub type OutboundPool<T: Config> =
+		StorageMap<_, Twox64Concat, PoolRound, Vec<UnboundedBytes>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::unbounded]
+	/// key: pool round
+	/// value: pending fee rate
+	pub type FeeRate<T: Config> =
+		StorageMap<_, Twox64Concat, PoolRound, PendingFeeRate<T::AccountId>, ValueQuery>;
 }
