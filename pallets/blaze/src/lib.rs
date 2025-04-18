86a9ca26151a83f9a1dc728e5fdb111709596866
Alex Won <dnjscksdn98@gmail.com>
2025-04-18 14:22:29 +0900

    NODE-180, feat: utxo submission (#113)
    * NODE-180, feat: impl verify_submit_utxos
    
    * NODE-180, fix: refactor utxo model
    
    * NODE-180, feat: impl submit_utxos
    
    * NODE-180, fix: continue instead of throwing an error
    
    * NODE-180, fix: use better signature
    
    * NODE-180, chore: update doc-strings
    
    * NODE-180, feat: impl spend_txos
    
    * NODE-180, fix: remove locktime
    
    * NODE-180, fix: update submission
    
    * NODE-181, feat: impl submit_outbound_requests (#114)
    
    * NODE-181, feat: impl submit_outbound_requests
    
    * NODE-182, feat: fee rate submission (#115)
    
    * NODE-181, feat: impl submit_fee_rate
    
    * NODE-181, chore: remove expired fee rates in on_initialize

--- pallets/blaze/src/lib.rs
+++ pallets/blaze/src/lib.rs
@@ -9,69 +9,42 @@ pub use weights::WeightInfo;
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
 use sp_core::{ConstU32, RuntimeDebug, H256, U256};
-use sp_runtime::BoundedBTreeMap;
+use sp_runtime::BoundedVec;
 use sp_std::vec::Vec;
 
 use bp_btc_relay::UnboundedBytes;
 use bp_staking::MAX_AUTHORITIES;
 
-/// The round of the registration pool.
-pub type PoolRound = u32;
-
 #[derive(Decode, Encode, TypeInfo, Clone, PartialEq, Eq, RuntimeDebug)]
 pub struct Utxo<AccountId> {
+	pub inner: UtxoInfo,
+	pub is_approved: bool,
+	pub voters: BoundedVec<AccountId, ConstU32<MAX_AUTHORITIES>>,
+}
+
+#[derive(Decode, Encode, TypeInfo, Clone, PartialEq, Eq, RuntimeDebug)]
+pub struct UtxoInfo {
 	pub txid: H256,
 	pub vout: U256,
 	pub amount: U256,
-	pub is_approved: bool,
-	/// If the locktime is less than 500 million, it's interpreted as a block height.
-	/// Otherwise, it's interpreted as a timestamp.
-	pub lock_time: U256,
-	pub votes: BoundedBTreeMap<AccountId, bool, ConstU32<MAX_AUTHORITIES>>,
-}
-
-#[derive(Decode, Encode, TypeInfo, Clone, PartialEq, Eq, RuntimeDebug)]
-pub struct PendingFeeRate<AccountId> {
-	pub is_approved: bool,
-	pub votes: BoundedBTreeMap<AccountId, U256, ConstU32<MAX_AUTHORITIES>>,
-}
-
-impl<AccountId: Ord> Default for PendingFeeRate<AccountId> {
-	fn default() -> Self {
-		Self { is_approved: false, votes: Default::default() }
-	}
 }
 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
 pub struct UtxoSubmission<AccountId> {
 	pub authority_id: AccountId,
-	pub utxos: Vec<Utxo<AccountId>>,
-	/// The pool round.
-	pub pool_round: PoolRound,
+	pub utxos: Vec<UtxoInfo>,
 }
 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
-pub struct FeeRateSubmission<AccountId> {
+pub struct FeeRateSubmission<AccountId, BlockNumber> {
 	pub authority_id: AccountId,
 	pub fee_rate: U256,
-	/// The pool round.
-	pub pool_round: PoolRound,
+	/// The deadline of the submission. Used to filter out expired signatures.
+	pub deadline: BlockNumber,
 }
 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
 pub struct OutboundRequestSubmission<AccountId> {
 	pub authority_id: AccountId,
 	pub messages: Vec<UnboundedBytes>,
-	/// The pool round.
-	pub pool_round: PoolRound,
-}
-
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
-pub struct SpendTxosSubmission<AccountId> {
-	pub authority_id: AccountId,
-	/// key: txid
-	/// value: vout
-	pub locked_txos: Vec<(H256, U256)>,
-	/// The pool round.
-	pub pool_round: PoolRound,
 }
--- pallets/blaze/src/pallet/impls.rs
+++ pallets/blaze/src/pallet/impls.rs
@@ -1,20 +1,56 @@
-use frame_support::pallet_prelude::{TransactionPriority, TransactionValidity, ValidTransaction};
+use bp_staking::traits::Authorities;
+use frame_support::pallet_prelude::{
+	InvalidTransaction, TransactionPriority, TransactionValidity, ValidTransaction,
+};
+use frame_system::pallet_prelude::BlockNumberFor;
+use parity_scale_codec::Encode;
+use scale_info::prelude::{format, string::String};
+use sp_core::H256;
+use sp_io::hashing::keccak_256;
+use sp_runtime::traits::{Block, Header, Verify};
+use sp_std::{fmt::Display, vec::Vec};
 
-use crate::{FeeRateSubmission, OutboundRequestSubmission, SpendTxosSubmission, UtxoSubmission};
+use crate::{FeeRateSubmission, OutboundRequestSubmission, UtxoSubmission};
 
 use super::pallet::*;
 
 impl<T: Config> Pallet<T> {
-	pub fn verify_submit_utxos(
+	pub fn verify_utxo_submission(
 		utxo_submission: &UtxoSubmission<T::AccountId>,
 		signature: &T::Signature,
+		tag_prefix: &'static str,
 	) -> TransactionValidity {
-		let UtxoSubmission { authority_id, utxos, pool_round } = utxo_submission;
+		let UtxoSubmission { authority_id, utxos: votes } = utxo_submission;
 
-		// TODO: verify authority
-		// TODO: verify signature
+		// verify if the authority is a selected relayer.
+		if !T::Relayers::is_authority(&authority_id) {
+			return Err(InvalidTransaction::BadSigner.into());
+		}
 
-		ValidTransaction::with_tag_prefix("UtxoSubmission")
+		// verify if the signature was originated from the authority.
+		let message = [
+			keccak_256(tag_prefix.as_bytes()).as_slice(),
+			format!(
+				"{}",
+				votes
+					.iter()
+					.map(|x| {
+						let utxo_hash = H256::from_slice(
+							keccak_256(&Encode::encode(&(x.txid, x.vout, x.amount))).as_ref(),
+						);
+						hex::encode(utxo_hash)
+					})
+					.collect::<Vec<String>>()
+					.concat()
+			)
+			.as_bytes(),
+		]
+		.concat();
+		if !signature.verify(&*message, &authority_id) {
+			return Err(InvalidTransaction::BadProof.into());
+		}
+
+		ValidTransaction::with_tag_prefix(tag_prefix)
 			.priority(TransactionPriority::MAX)
 			.and_provides(authority_id)
 			.propagate(true)
@@ -22,17 +58,34 @@ impl<T: Config> Pallet<T> {
 	}
 
 	pub fn verify_submit_fee_rate(
-		fee_rate_submission: &FeeRateSubmission<T::AccountId>,
+		fee_rate_submission: &FeeRateSubmission<T::AccountId, BlockNumberFor<T>>,
 		signature: &T::Signature,
-	) -> TransactionValidity {
-		let FeeRateSubmission { authority_id, fee_rate, pool_round } = fee_rate_submission;
+	) -> TransactionValidity
+	where
+		<<<T as frame_system::Config>::Block as Block>::Header as Header>::Number: Display,
+	{
+		let FeeRateSubmission { authority_id, fee_rate, deadline } = fee_rate_submission;
 
-		// TODO: verify authority
-		// TODO: verify signature
+		// verify if the authority is a selected relayer.
+		if !T::Relayers::is_authority(&authority_id) {
+			return Err(InvalidTransaction::BadSigner.into());
+		}
+
+		// verify if the deadline is not expired.
+		let now = <frame_system::Pallet<T>>::block_number();
+		if now > *deadline {
+			return Err(InvalidTransaction::Stale.into());
+		}
+
+		// verify if the signature was originated from the authority.
+		let message = format!("{}:{}", deadline, fee_rate);
+		if !signature.verify(message.as_bytes(), &authority_id) {
+			return Err(InvalidTransaction::BadProof.into());
+		}
 
 		ValidTransaction::with_tag_prefix("FeeRateSubmission")
 			.priority(TransactionPriority::MAX)
-			.and_provides(authority_id)
+			.and_provides((authority_id, fee_rate))
 			.propagate(true)
 			.build()
 	}
@@ -41,29 +94,27 @@ impl<T: Config> Pallet<T> {
 		outbound_request_submission: &OutboundRequestSubmission<T::AccountId>,
 		signature: &T::Signature,
 	) -> TransactionValidity {
-		let OutboundRequestSubmission { authority_id, messages, pool_round } =
-			outbound_request_submission;
+		let OutboundRequestSubmission { authority_id, messages } = outbound_request_submission;
 
-		// TODO: verify authority
-		// TODO: verify signature
+		// verify if the authority is a selected relayer.
+		if !T::Relayers::is_authority(&authority_id) {
+			return Err(InvalidTransaction::BadSigner.into());
+		}
+
+		// verify if the signature was originated from the authority.
+		let message = format!(
+			"{}",
+			messages
+				.iter()
+				.map(|x| array_bytes::bytes2hex("0x", x))
+				.collect::<Vec<String>>()
+				.concat()
+		);
+		if !signature.verify(message.as_bytes(), &authority_id) {
+			return Err(InvalidTransaction::BadProof.into());
+		}
 
 		ValidTransaction::with_tag_prefix("OutboundRequestSubmission")
-			.priority(TransactionPriority::MAX)
-			.and_provides(authority_id)
-			.propagate(true)
-			.build()
-	}
-
-	pub fn verify_spend_txos(
-		spend_txos_submission: &SpendTxosSubmission<T::AccountId>,
-		signature: &T::Signature,
-	) -> TransactionValidity {
-		let SpendTxosSubmission { authority_id, locked_txos, pool_round } = spend_txos_submission;
-
-		// TODO: verify authority
-		// TODO: verify signature
-
-		ValidTransaction::with_tag_prefix("SpendTxosSubmission")
 			.priority(TransactionPriority::MAX)
 			.and_provides(authority_id)
 			.propagate(true)
