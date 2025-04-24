c48ed3404c9cb8c9ab7c833cd51e08e2b72f50eb
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-04-24 13:01:47 +0900

    NODE-185, deps: increase testnet runtime version

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -152,7 +152,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 484,
+	spec_version: 485,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
