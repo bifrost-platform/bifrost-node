3d4a09013739301bafcfe8ada0d81af1c57bd469
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-05-22 16:11:06 +0900

    deps: cleanup crate dependencies

--- node/testnet/src/service.rs
+++ node/testnet/src/service.rs
@@ -44,10 +44,6 @@ pub mod testnet {
 
 	pub struct ExecutorDispatch;
 	impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
-		#[cfg(feature = "runtime-benchmarks")]
-		type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
-
-		#[cfg(not(feature = "runtime-benchmarks"))]
 		type ExtendHostFunctions = fp_ext::bifrost_ext::HostFunctions;
 
 		fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
