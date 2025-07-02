e9132ec16b40b8f1dfeb6a46b3d58678b6e5b01c
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-07-02 17:29:21 +0900

    increase runtime version (488)

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -153,7 +153,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 487,
+	spec_version: 488,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
