e991de98c58e8a02cd4d560ac667bbc7916ea278
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-07-28 16:49:32 +0900

    NODE-161, fix: remove Paramter

--- pallets/btc-registration-pool/src/pallet/mod.rs
+++ pallets/btc-registration-pool/src/pallet/mod.rs
@@ -43,11 +43,7 @@ pub mod pallet {
 		/// The signature signed by the issuer.
 		type Signature: Verify<Signer = Self::Signer> + Encode + Decode + Parameter;
 		/// The signer of the message.
-		type Signer: IdentifyAccount<AccountId = Self::AccountId>
-			+ Encode
-			+ Decode
-			+ Parameter
-			+ MaxEncodedLen;
+		type Signer: IdentifyAccount<AccountId = Self::AccountId> + Encode + Decode + MaxEncodedLen;
 		/// The relay executive members.
 		type Executives: SortedMembers<Self::AccountId>;
 		/// Interface of Bitcoin Socket Queue pallet.
