9c7635d003ce999388809e5961c8f72ac3368d56
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-08-20 12:17:47 +0900

    NODE-161, chore: add pallet_session migration

--- runtime/dev/src/lib.rs
+++ runtime/dev/src/lib.rs
@@ -116,7 +116,12 @@ pub type UncheckedExtrinsic =
 	fp_self_contained::UncheckedExtrinsic<Address, RuntimeCall, Signature, TxExtension>;
 
 /// All migrations executed on runtime upgrade as a nested tuple of types implementing `OnRuntimeUpgrade`.
-type Migrations = ();
+type Migrations = (
+	pallet_session::migrations::v1::MigrateV0ToV1<
+		Runtime,
+		pallet_session::migrations::v1::InitOffenceSeverity<Runtime>,
+	>,
+);
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
