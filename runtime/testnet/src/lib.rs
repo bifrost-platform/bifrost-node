25613c5d3fc8c751881c743cc6119356c3b5b0f4
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-10-16 16:42:02 +0900

    increase testnet runtime to v494

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -157,7 +157,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 493,
+	spec_version: 494,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
