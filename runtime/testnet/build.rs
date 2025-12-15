ebf3c2d533a93d9d17a6f9455437f20669c887c8
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-12-15 18:16:30 +0900

    fix: use correct token symbal and decimals

--- runtime/testnet/build.rs
+++ runtime/testnet/build.rs
@@ -4,7 +4,7 @@ fn main() {
 
 	#[cfg(all(feature = "std", feature = "metadata-hash"))]
 	substrate_wasm_builder::WasmBuilder::init_with_defaults()
-		.enable_metadata_hash("UNIT", 12)
+		.enable_metadata_hash("BFC", 18)
 		.build();
 
 	#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
