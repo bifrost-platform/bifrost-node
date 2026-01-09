966bac19e8d87759e272787b125563dcd9099cb1
dnjscksdn98 <dnjscksdn98@gmail.com>
2026-01-09 13:41:32 +0900

    NODE-197, feat: complete finalize_poll()

--- pallets/cccp-transfer-manager/src/pallet/mod.rs
+++ pallets/cccp-transfer-manager/src/pallet/mod.rs
@@ -2,7 +2,7 @@ mod impls;
 
 use crate::{
 	weights::WeightInfo, AssetCapInfo, AssetId, AssetIndexHash, BalanceOf, SocketMessageSubmission,
-	TransferInfo, TransferOption,
+	TransferInfo, TransferOption, TransferStatus,
 };
 
 use frame_support::{
@@ -60,8 +60,12 @@ pub mod pallet {
 		SocketDNE,
 		/// The transfer is already finalized.
 		TransferAlreadyFinalized,
-		/// The transfer is already approved.
-		TransferAlreadyApproved,
+		/// The transfer is already on flight.
+		TransferAlreadyOnFlight,
+		/// The transfer is not on flight.
+		TransferNotOnFlight,
+		/// The transfer does not exist.
+		TransferDNE,
 		/// The authority has already voted.
 		AlreadyVoted,
 		/// The value is out of range.
@@ -78,7 +82,14 @@ pub mod pallet {
 			authority_id: T::AccountId,
 			option: TransferOption,
 			amount: BalanceOf<T>,
-			is_approved: bool,
+			status: TransferStatus,
+		},
+		/// A finalization has been polled.
+		FinalizationPolled {
+			asset_index_hash: AssetIndexHash,
+			sequence_id: U256,
+			authority_id: T::AccountId,
+			is_finalized: bool,
 		},
 	}
 
@@ -141,7 +152,7 @@ pub mod pallet {
 	{
 		#[pallet::call_index(0)]
 		#[pallet::weight(<T as Config>::WeightInfo::default())]
-		pub fn poll(
+		pub fn on_flight_poll(
 			origin: OriginFor<T>,
 			socket_message_submission: SocketMessageSubmission<T::AccountId>,
 			_signature: T::Signature,
@@ -190,7 +201,7 @@ pub mod pallet {
 				let request_info = Self::try_get_request(&msg.encode_req_id())?;
 				ensure!(request_info.is_msg_hash(msg_hash), Error::<T>::InvalidSocketMessage);
 				ensure!(request_info.is_requested(), Error::<T>::InvalidSocketMessage);
-				ensure!(on_flight_transfer.is_none(), Error::<T>::TransferAlreadyApproved);
+				ensure!(on_flight_transfer.is_none(), Error::<T>::TransferAlreadyOnFlight);
 
 				// if the asset cap is exceeded, the transfer will fallback to standard-transfer.
 				let option = if cap_after > asset_cap.max_on_flight_cap.into() {
@@ -205,10 +216,11 @@ pub mod pallet {
 					TransferInfo {
 						amount,
 						option,
+						status: TransferStatus::OnFlight,
 						socket_message: message.clone(),
-						voters: BoundedVec::try_from(vec![authority_id.clone()])
+						on_flight_voters: BoundedVec::try_from(vec![authority_id.clone()])
 							.map_err(|_| Error::<T>::OutOfRange)?,
-						is_approved: true,
+						finalization_voters: BoundedVec::new(),
 					},
 				);
 
@@ -225,28 +237,31 @@ pub mod pallet {
 					authority_id,
 					option,
 					amount,
-					is_approved: true,
+					status: TransferStatus::OnFlight,
 				});
 			} else {
 				// for inbound requests, we require voting
 				if let Some(mut on_flight_transfer) = on_flight_transfer {
-					ensure!(!on_flight_transfer.is_approved, Error::<T>::TransferAlreadyApproved);
+					ensure!(
+						on_flight_transfer.status == TransferStatus::Pending,
+						Error::<T>::TransferAlreadyOnFlight
+					);
 					ensure!(
 						on_flight_transfer.socket_message == message,
 						Error::<T>::InvalidSocketMessage
 					);
 					ensure!(
-						!on_flight_transfer.voters.contains(&authority_id),
+						!on_flight_transfer.on_flight_voters.contains(&authority_id),
 						Error::<T>::AlreadyVoted
 					);
 					on_flight_transfer
-						.voters
+						.on_flight_voters
 						.try_push(authority_id.clone())
 						.map_err(|_| Error::<T>::OutOfRange)?;
 
 					// check if the transfer is approved
-					if on_flight_transfer.voters.len() as u32 >= T::Relayers::majority() {
-						on_flight_transfer.is_approved = true;
+					if on_flight_transfer.on_flight_voters.len() as u32 >= T::Relayers::majority() {
+						on_flight_transfer.status = TransferStatus::OnFlight;
 
 						// increase the asset cap if fast transfer
 						if on_flight_transfer.option == TransferOption::Fast {
@@ -261,7 +276,7 @@ pub mod pallet {
 						authority_id,
 						option: on_flight_transfer.option,
 						amount: on_flight_transfer.amount,
-						is_approved: on_flight_transfer.is_approved,
+						status: on_flight_transfer.status,
 					});
 					OnFlightTransfers::<T>::insert(
 						asset_index_hash,
@@ -281,10 +296,11 @@ pub mod pallet {
 						TransferInfo {
 							amount,
 							option,
+							status: TransferStatus::Pending,
 							socket_message: message.clone(),
-							voters: BoundedVec::try_from(vec![authority_id.clone()])
+							on_flight_voters: BoundedVec::try_from(vec![authority_id.clone()])
 								.map_err(|_| Error::<T>::OutOfRange)?,
-							is_approved: false,
+							finalization_voters: BoundedVec::new(),
 						},
 					);
 					Self::deposit_event(Event::TransferPolled {
@@ -293,7 +309,7 @@ pub mod pallet {
 						authority_id,
 						option,
 						amount,
-						is_approved: false,
+						status: TransferStatus::Pending,
 					});
 				}
 			}
@@ -303,11 +319,151 @@ pub mod pallet {
 
 		#[pallet::call_index(1)]
 		#[pallet::weight(<T as Config>::WeightInfo::default())]
-		pub fn finalize(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
-			// TODO: this function can handle both committed and rollbacked transfers.
+		pub fn finalize_poll(
+			origin: OriginFor<T>,
+			socket_message_submission: SocketMessageSubmission<T::AccountId>,
+			_signature: T::Signature,
+		) -> DispatchResultWithPostInfo {
+			ensure_none(origin)?;
 
-			// TODO: move the transfer from OnFlightTransfers to FinalizedTransfers
-			// TODO: decrease AssetCap.on_flight_cap
+			let SocketMessageSubmission { authority_id, message } = socket_message_submission;
+			ensure!(!message.is_empty(), Error::<T>::EmptySubmission);
+
+			// the bytes should be a valid socket message
+			let msg = SocketMessage::try_from(message.clone())
+				.map_err(|_| Error::<T>::InvalidSocketMessage)?;
+			ensure!(msg.is_committed() || msg.is_rollbacked(), Error::<T>::InvalidSocketMessage);
+			let amount = msg.params.amount.try_into().map_err(|_| Error::<T>::OutOfRange)?;
+
+			// the bridge asset must be in AssetIndexes & AssetCaps
+			let asset_index_hash = AssetIndexHash::from_slice(&msg.params.token_idx0);
+			let asset_id =
+				AssetIndexes::<T>::get(asset_index_hash).ok_or(Error::<T>::UnknownAssetIndex)?;
+			let mut asset_cap =
+				AssetCaps::<T>::get(asset_id).ok_or(Error::<T>::UnknownAssetAddress)?;
+
+			// the transfer must not be finalized yet
+			ensure!(
+				!FinalizedTransfers::<T>::contains_key(asset_index_hash, msg.req_id.sequence),
+				Error::<T>::TransferAlreadyFinalized
+			);
+			// the transfer must exist in OnFlightTransfers and approved
+			let mut on_flight_transfer =
+				OnFlightTransfers::<T>::get(asset_index_hash, msg.req_id.sequence)
+					.ok_or(Error::<T>::TransferDNE)?;
+			ensure!(
+				on_flight_transfer.status == TransferStatus::OnFlight,
+				Error::<T>::TransferNotOnFlight
+			);
+			// the submitted socket message must be the same as the one in OnFlightTransfers (except for the status)
+			let mut initial_socket_message =
+				SocketMessage::try_from(on_flight_transfer.socket_message.clone())
+					.map_err(|_| Error::<T>::InvalidSocketMessage)?;
+			initial_socket_message.status = msg.status;
+			ensure!(initial_socket_message.encode() == message, Error::<T>::InvalidSocketMessage);
+
+			// the message must be valid onchain
+			let msg_hash = Self::hash_bytes(
+				&UserRequest::new(msg.ins_code.clone(), msg.params.clone()).encode(),
+			);
+			let request_info = Self::try_get_request(&msg.encode_req_id())?;
+			ensure!(request_info.is_msg_hash(msg_hash), Error::<T>::InvalidSocketMessage);
+
+			ensure!(
+				!on_flight_transfer.finalization_voters.contains(&authority_id),
+				Error::<T>::AlreadyVoted
+			);
+			on_flight_transfer
+				.finalization_voters
+				.try_push(authority_id.clone())
+				.map_err(|_| Error::<T>::OutOfRange)?;
+
+			if msg.is_outbound(<T as pallet_evm::Config>::ChainId::get() as u32) {
+				// for outbound requests, after commit/rollback, the status on Bifrost must be Committed|Rollbacked
+				ensure!(
+					(request_info.is_committed() && msg.is_committed())
+						|| (request_info.is_rollbacked() && msg.is_rollbacked()),
+					Error::<T>::InvalidSocketMessage
+				);
+
+				// for outbound requests, if the socket message status on Bifrost is committed, the transfer will be finalized immediately.
+				on_flight_transfer.status = TransferStatus::Finalized;
+				FinalizedTransfers::<T>::insert(
+					asset_index_hash,
+					msg.req_id.sequence,
+					on_flight_transfer.clone(),
+				);
+				OnFlightTransfers::<T>::remove(asset_index_hash, msg.req_id.sequence);
+
+				if on_flight_transfer.option == TransferOption::Fast {
+					// calculate the new on-flight cap
+					let cap_after = asset_cap
+						.on_flight_cap
+						.into()
+						.checked_sub(amount)
+						.ok_or(Error::<T>::OutOfRange)?;
+					asset_cap.on_flight_cap =
+						cap_after.try_into().map_err(|_| Error::<T>::OutOfRange)?;
+					AssetCaps::<T>::insert(asset_id, asset_cap);
+				}
+
+				Self::deposit_event(Event::FinalizationPolled {
+					asset_index_hash,
+					sequence_id: msg.req_id.sequence,
+					authority_id,
+					is_finalized: true,
+				});
+				return Ok(().into());
+			} else {
+				// for inbound requests, after commit/rollback, the status on Bifrost must be Accepted|Rejected
+				ensure!(
+					(request_info.is_accepted() && msg.is_committed())
+						|| (request_info.is_rejected() && msg.is_rollbacked()),
+					Error::<T>::InvalidSocketMessage
+				);
+			}
+
+			if on_flight_transfer.finalization_voters.len() as u32 >= T::Relayers::majority() {
+				on_flight_transfer.status = TransferStatus::Finalized;
+				FinalizedTransfers::<T>::insert(
+					asset_index_hash,
+					msg.req_id.sequence,
+					on_flight_transfer.clone(),
+				);
+				OnFlightTransfers::<T>::remove(asset_index_hash, msg.req_id.sequence);
+
+				if on_flight_transfer.option == TransferOption::Fast {
+					// calculate the new on-flight cap
+					let cap_after = asset_cap
+						.on_flight_cap
+						.into()
+						.checked_sub(amount)
+						.ok_or(Error::<T>::OutOfRange)?;
+					asset_cap.on_flight_cap =
+						cap_after.try_into().map_err(|_| Error::<T>::OutOfRange)?;
+					AssetCaps::<T>::insert(asset_id, asset_cap);
+				}
+
+				Self::deposit_event(Event::FinalizationPolled {
+					asset_index_hash,
+					sequence_id: msg.req_id.sequence,
+					authority_id,
+					is_finalized: true,
+				});
+			} else {
+				OnFlightTransfers::<T>::insert(
+					asset_index_hash,
+					msg.req_id.sequence,
+					on_flight_transfer.clone(),
+				);
+				Self::deposit_event(Event::FinalizationPolled {
+					asset_index_hash,
+					sequence_id: msg.req_id.sequence,
+					authority_id,
+					is_finalized: false,
+				});
+			}
+
 			Ok(().into())
 		}
 	}
@@ -323,18 +479,37 @@ pub mod pallet {
 
 		fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
 			match call {
-				Call::poll { socket_message_submission, signature } => {
+				Call::on_flight_poll { socket_message_submission, signature } => {
 					let SocketMessageSubmission { authority_id, message } =
 						socket_message_submission;
 					Self::verify_authority(authority_id)?;
 
 					// verify if the signature was originated from the authority_id.
-					let message = [keccak_256("Poll".as_bytes()).as_slice(), message].concat();
+					let message =
+						[keccak_256("OnFlightPoll".as_bytes()).as_slice(), message].concat();
 					if !signature.verify(&*message, authority_id) {
 						return InvalidTransaction::BadProof.into();
 					}
 
-					ValidTransaction::with_tag_prefix("Poll")
+					ValidTransaction::with_tag_prefix("OnFlightPoll")
+						.priority(TransactionPriority::MAX)
+						.and_provides((authority_id, signature))
+						.propagate(true)
+						.build()
+				},
+				Call::finalize_poll { socket_message_submission, signature } => {
+					let SocketMessageSubmission { authority_id, message } =
+						socket_message_submission;
+					Self::verify_authority(authority_id)?;
+
+					// verify if the signature was originated from the authority_id.
+					let message =
+						[keccak_256("FinalizePoll".as_bytes()).as_slice(), message].concat();
+					if !signature.verify(&*message, authority_id) {
+						return InvalidTransaction::BadProof.into();
+					}
+
+					ValidTransaction::with_tag_prefix("FinalizePoll")
 						.priority(TransactionPriority::MAX)
 						.and_provides((authority_id, signature))
 						.propagate(true)
