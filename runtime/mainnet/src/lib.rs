3ad31a0cca91fd6c5370a362302e9ac00c712cd9
dnjscksdn98 <dnjscksdn98@gmail.com>
2026-03-18 17:20:11 +0900

    chore: apply FeelessCallFilter to mainnet runtime

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -1023,8 +1023,7 @@ impl pallet_evm::Config for Runtime {
 	type Timestamp = Timestamp;
 	type CreateInnerOriginFilter = ();
 	type CreateOriginFilter = ();
-	// TODO: Enable BifrostFeelessCalls<Runtime> when pallet_bifrost_evm_tx_payment is added
-	type FeelessCallFilter = ();
+	type FeelessCallFilter = bifrost_common_runtime::BifrostFeelessCalls<Runtime>;
 	type WeightInfo = pallet_evm::weights::SubstrateWeight<Runtime>;
 }
 
