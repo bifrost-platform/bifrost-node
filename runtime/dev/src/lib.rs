fedb68d80f45f289f173e70e99aecc31851b0dc1
dnjscksdn98 <dnjscksdn98@gmail.com>
2026-03-25 13:14:11 +0900

    chore(deps): increase testbed runtime version v447 and apply missing blaze logic

--- runtime/dev/src/lib.rs
+++ runtime/dev/src/lib.rs
@@ -158,7 +158,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 446,
+	spec_version: 447,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
