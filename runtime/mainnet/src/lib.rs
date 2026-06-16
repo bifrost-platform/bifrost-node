939a8b935c4432093bbbe83825adb12bf4f17c69
Alex Won <dnjscksdn98@gmail.com>
2026-06-16 12:07:24 +0900

    Ensure eoa caller to BfcStaking.sol (#207)
    * fix: ensure eoa caller to BfcStaking.sol
    
    * chore(deps): release mainnet runtime v2043
    
    * feat: implement custom BifrostPrecompiles to block calls to blocked accounts
    
    * feat: migrate bfc-utility to v4
    
    * fix: adjust execute_leave_candidates weight

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -158,7 +158,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the authorship interface.
 	authoring_version: 1,
 	// The version of the runtime spec.
-	spec_version: 2042,
+	spec_version: 2044,
 	// The version of the implementation of the spec.
 	impl_version: 1,
 	// A list of supported runtime APIs along with their versions.
